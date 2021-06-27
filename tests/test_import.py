ccf7d4a3e2cdfbc18db972a3a41a2ed4ae4dc7bf
zafraan <zafraanmohd@gmail.com>
2021-06-28 04:11:34 +0530

    removing unusable compenents

--- tests/test_import.py
+++
@@ -1,9 +0,0 @@
-import os , sys
-sys.path.append(os.getcwd())
-import pytest
-from ThreadPoolExecutorPlus import *
-
-@pytest.mark.asyncio
-async def test_import():
-    ThreadPoolExecutor()
-    ...
